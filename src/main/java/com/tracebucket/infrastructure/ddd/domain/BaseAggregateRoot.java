860228610f5a1649480fe2fe810b534008773bde
Firoz Fazil <firoz.fazil@gmail.com>
2015-02-05 18:49:47 +0530

    Update BaseAggregateRoot.java

--- src/main/java/com/tracebucket/infrastructure/ddd/domain/BaseAggregateRoot.java
+++ src/main/java/com/tracebucket/infrastructure/ddd/domain/BaseAggregateRoot.java
@@ -1,21 +1,3 @@
-/*
- * Copyright 2011-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * 
- */
 package com.tracebucket.infrastructure.ddd.domain;
 
 import com.tracebucket.infrastructure.ddd.exception.DomainOperationException;
@@ -29,6 +11,10 @@ import reactor.event.Event;
 
 import javax.persistence.*;
 import java.util.*;
+
+/**
+ * @author FFL
+ */
 
 @Configurable
 @Scope("prototype")//created in domain factories, not in spring container, therefore we don't want eager creation
